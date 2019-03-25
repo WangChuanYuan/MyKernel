79359d304e62e615cd919f2e1246ea4440f2b315
161250135 <161250135@smail.nju.edu.cn>
2019-03-25 09:45:14 +0800

    wc finish

--- wc.c
+++ wc.c
@@ -1,23 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
-#define MAX_PATH_COUNT 256
+#define BUFFER_SIZE 1024
 #define MAX_PATH_LEN 256
 
 typedef struct counter {
     unsigned long line_count;
     unsigned long word_count;
     unsigned long byte_count;
+    int is_dir;
     char target[MAX_PATH_LEN];
 } counter_t;
 
 void print(counter_t *counter) {
-    printf("%10lu %10lu %10lu %s\n", counter->line_count, counter->word_count, counter->byte_count, counter->target);
+    if (counter->is_dir)
+        printf("wc: %s: Is a directory\n", counter->target);
+    printf("%6lu\t %6lu\t %6lu\t %s\n", counter->line_count, counter->word_count, counter->byte_count, counter->target);
 }
 
-void wc(char *file, counter_t *counter) {
+int wc(char *file, counter_t *counter) {
+    counter->line_count = 0;
+    counter->word_count = 0;
+    counter->byte_count = 0;
+    strcpy(counter->target, file);
 
+    struct stat info;
+    if (stat(counter->target, &info) == -1)
+        return 0;
+    else if (S_ISDIR(info.st_mode)) {
+        counter->is_dir = 1;
+        return 1;
+    }
+
+    FILE *fp = fopen(file, "r");
+    if (fp == NULL) return 0;
+    char line[BUFFER_SIZE];
+    while (fgets(line, BUFFER_SIZE, fp) != NULL) {
+        int is_word_char = 0;
+        for (int i = 0; i < strlen(line); i++) {
+            char c = line[i];
+            counter->byte_count++;
+            if (c == '\n') {
+                counter->line_count++;
+                if (is_word_char) counter->word_count++;
+                is_word_char = 0;
+            } else if (c == ' ' || c == '\t') {
+                if (is_word_char) counter->word_count++;
+                is_word_char = 0;
+            } else is_word_char = 1;
+        }
+    }
+    return 1;
 }
 
 int main(int argc, char **argv) {
@@ -25,13 +60,15 @@ int main(int argc, char **argv) {
     counter_t *counters = (counter_t *) malloc(num * sizeof(counter_t));
 
     if (counters != NULL) {
-        counter_t total = {.line_count = 0, .word_count = 0, .byte_count = 0};
+        counter_t total = {.line_count = 0, .word_count = 0, .byte_count = 0, .is_dir = 0};
         strcpy(total.target, "total");
 
         for (int i = 1; i < argc; i++) {
             counter_t *curr_counter = counters + i - 1;
-            wc(argv[i], curr_counter);
-            print(curr_counter);
+            if (wc(argv[i], curr_counter))
+                print(curr_counter);
+            else
+                fprintf(stderr, "wc: cannot open '%s'\n", argv[i]);
             total.line_count += curr_counter->line_count;
             total.word_count += curr_counter->word_count;
             total.byte_count += curr_counter->byte_count;
